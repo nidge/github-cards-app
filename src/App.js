cfaadf43174b86980a15d68a4b9b187d5a227f72
Nigel <nigelsmyth@hotmail.com>
2022-04-28 16:55:36 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+ 
+const axios = require('axios').default;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+  	this.setState(prevState => ({
+    	profiles: [...prevState.profiles, profileData],
+    }));
+  };
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+    	</div>
+    );
+  }	
 }
+
+// GitHub usernames: gaearon, sophiebits, sebmarkbage, bvaughn
+const CardList = (props) => (
+	<div>
+  	{props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+	</div>
+);
+
+
+class Card extends React.Component {
+	render() {
+  	const profile = this.props;
+  	return (
+    	<div className="github-profile">
+    	  <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+    	</div>
+    );
+  }
+}
+
+class Form extends React.Component {
+	state = { userName: '' };
+	handleSubmit = async (event) => {
+  	event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+ // const resp = await axios.get(`https://api.postcodes.io/postcodes/LS28 7RL`);
+
+ 
+    console.log(resp);
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: '' });
+  };
+	render() {
+  	return (
+    	<form onSubmit={this.handleSubmit}>
+    	  <input 
+          type="text" 
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username" 
+          required 
+        />
+        <button>Add card</button>
+    	</form>
+    );
+  }
+}
+
+<div id="app" />
 
 export default App;
